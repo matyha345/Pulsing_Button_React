98f949deb01e7062c42f7bd1487c61286bafb97a
Nikolay Matyukhin <123973210+matyha345@users.noreply.github.com>
2023-06-29 11:52:04 +0300

    Button & Video
    Шпаргалка для Play video & + PlayButton

---
+++ React_video/BenefitVideo.jsx
@@ -0,0 +1,47 @@
+import styles from './BenefitVideo.module.scss'
+
+import imgVideo from './Photo.jpg'
+
+import pathVideo from './videoOne.mp4'
+
+
+import { useRef, useState } from 'react'
+import PlayButton from '../../../../ui/play_button/PlayButton'
+
+const BenefitVideo = () => {
+	const [isPlaying, setIsPlaying] = useState(false)
+
+	const videoRef = useRef(null)
+
+	const handlerPlay = () => {
+		setIsPlaying(true)
+		videoRef.current.play()
+	}
+
+	return (
+		<div className={styles.wrapper}>
+			<div className='container'>
+				<div className={styles.content}>
+					<div className={styles.videos}>
+						<video
+							className={styles.video}
+							src={pathVideo}
+							width={618}
+							height={361}
+							poster={imgVideo}
+							ref={videoRef}
+							onPlay={() => setIsPlaying(true)}
+							onPause={() => setIsPlaying(false)}
+						></video>
+						{!isPlaying && (
+							<button className={styles.playButton} onClick={handlerPlay}>
+								<PlayButton />
+							</button>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+export default BenefitVideo
